0306e7132b11f880df6afea8a045df8c7ee18599
Ryan Hileman <lunixbochs@gmail.com>
2013-07-05 18:47:58 -0700

    nevermind, transparently serve form through /p/

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask('paste')
 mongo = PyMongo(app)
 
 
+@app.route('/p/')
 @app.route('/', methods=['GET', 'POST'])
 def slash():
     if request.method == 'GET':
@@ -25,11 +26,8 @@ def slash():
             return abort(419)
 
 
-@app.route('/p/')
 @app.route('/p/<ObjectId:paste_id>.txt')
-def get(paste_id=None):
-    if not paste_id:
-        return redirect('/')
+def get(paste_id):
     paste = mongo.db.paste.find_one_or_404({'_id': paste_id})
     return Response(paste['data'], mimetype='text/plain')
 
