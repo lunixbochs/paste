f06e5c1395c7c8020fefa727d3a4945e361b2d92
Ryan Hileman <lunixbochs@gmail.com>
2013-07-08 21:06:47 -0700

    reduce code; add short urls

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
 from flask.ext.pymongo import PyMongo
-from flask import (
-    Flask,
-    Response,
-    abort,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
+from flask.ext.shorturl import ShortUrl
+from flask import Flask, Response, abort, redirect, render_template, request
 
 app = Flask('paste')
 mongo = PyMongo(app)
+surl = ShortUrl(app)
 
 
-@app.route('/p/api/', methods=['POST'])
-def api_post():
-    data = request.form.get('data') or request.data
-    if data:
-        _id = mongo.db.paste.insert({'data': data})
-        return '/p/{0}.txt'.format(_id)
-    else:
-        return ''
+def next_id():
+    return mongo.db.auto_id.find_and_modify({},
+        {'$inc': {'next_id': 1}}, new=True, upsert=True)['next_id']
 
 
 @app.route('/p/', methods=['GET', 'POST'])
@@ -29,18 +18,20 @@ def slash():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        url = api_post()
-        if url:
-            return redirect(url)
+        data = request.form.get('data') or request.data
+        if data:
+            _id = surl.encode_url(next_id())
+            mongo.db.paste.insert({'_id': _id, 'data': data})
+            return redirect('/p/{0}'.format(_id))
         else:
             return abort(400)
 
 
-@app.route('/p/<ObjectId:paste_id>.txt')
-def get(paste_id):
-    paste = mongo.db.paste.find_one_or_404({'_id': paste_id})
+@app.route('/p/<ObjectId:_id>.txt')
+@app.route('/p/<_id>')
+def get(_id):
+    paste = mongo.db.paste.find_one_or_404({'_id': _id})
     return Response(paste['data'], mimetype='text/plain')
-
 
 if __name__ == '__main__':
     app.run(port=5005)
